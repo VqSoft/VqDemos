3629cfdd5230d03dc10f15853178b08bed1938f7
Eric.Zhao <Eric.Zhao@activenetwork.com>
2016-01-19 13:28:28 +0800

    Add new test class.

---
+++ XUnit.Demo/Testoutput/TestoutputTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace XUnit.Demo.Testoutput
+{
+    public class TestoutputTest
+    {
+        ITestOutputHelper output;
+
+        public TestoutputTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void TestThis()
+        {
+            output.WriteLine("I'm inside the test!");
+        }
+    }
+}
