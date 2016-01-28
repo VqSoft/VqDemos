2a957acd20747ad67e0ede0ec2dda17f3686d732
Eric.Zhao <Eric.Zhao@activenetwork.com>
2016-01-28 14:41:18 +0800

    Add mongo demos.

---
+++ MongoConsoleDemo/MongoDemos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Shared;
+
+namespace MongoConsoleDemo
+{
+    public sealed class MongoDemos
+    {
+        private const string CONN_STRING = "mongodb://localhost:27017/?readpreference=primaryPreferred";
+
+        public static void CollectionDemo()
+        {
+            var conn = new MongoClient(CONN_STRING);
+            var db = conn.GetDatabase("ZYDB");
+            var col = db.GetCollection<BsonDocument>("Col1");
+            var doc = new BsonDocument { 
+                {"Name" , "Zhaoy"},
+                {"Age" , 100}
+            };
+
+            col.InsertOne(doc);
+
+
+        }
+
+    }//class
+}
