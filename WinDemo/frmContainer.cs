7c0ffb9e93262849de62c727146b11851dc30b25
Eric.Zhao <Eric.Zhao@activenetwork.com>
2016-02-02 14:53:10 +0800

    Add mongodb winform

---
+++ WinDemo/frmContainer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinDemo
+{
+    public partial class frmContainer : Form
+    {
+
+        #region 子窗体
+        private static frmMongoDBTest mongodbTestForm = null;
+
+        #endregion
+        public frmContainer()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void mongoDBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mongodbTestForm == null || mongodbTestForm.IsDisposed)
+            {
+                mongodbTestForm = new frmMongoDBTest();
+                mongodbTestForm.MdiParent = this;
+                mongodbTestForm.WindowState = FormWindowState.Maximized;
+            }
+            mongodbTestForm.Show();
+        }
+    }
+}
